789191c6c51a650944ebde4c2b41b94f029b0199
Shimi22 <220034ob@yse-c.net>
2023-10-18 12:10:54 +0900

    main

--- express_sample/server.js
+++ express_sample/server.js
@@ -1,65 +1,34 @@
-//express読み込み
+// expressモジュール読み込み
 const express = require('express')
-//dotenv読み込み
+// dotenvモジュール読み込み
 const dotenv = require('dotenv')
+// routerモジュール読み込み
+const routes = require('./routes')
 
-//dotenv設定読み込み
+// dotenvの設定読み込み
 dotenv.config()
 const HOST = process.env.HOST
 const PORT = process.env.PORT
 
-console.log(HOST)
-console.log(PORT)
-
-//サーバ作成
+// サーバ作成
 const app = express()
 
-//ミドルウェアの設定
+// ミドルウェアの設定
+// publicフォルダを静的コンテンツのフォルダに設定
 app.use(express.static(__dirname + '/public'))
 
 // URLエンコード
 app.use(express.urlencoded({ extended: true }))
 
-//GETリクエスト処理
-app.get('/', (req,res) =>{
-    //リクエスト処理
-    console.log(req.body)
-    console.log(req.url)
-    console.log(req,query)
+// EJSをテンプレートエンジンとして設定
+app.set('view engine', 'ejs')
 
-    //レスポンス処理
-    res.send('Hello!!')
-})
+// ルーティングを有効
+app.use(routes)
 
-app.get('/profile', (req, res) => {
-    res.send('Profile Page')
-})
-
-// POSTリクエスト
-app.post('/auth', (req, res) => {
-    // POSTデータ取得
-    var loginName = req.body.login_name
-    var password = req.body.password
-    console.log(loginName, password)
-
-    var message = "ログイン失敗"
-    // .env で設定した値で、ログインチェック
-    // TODO：データベースに接続してユーザ取得
-    // TODO：パスワードはハッシュ値でチェック
-    if (loginName == process.env.LOGIN_NAME
-        && password == process.env.PASSWORD) {
-            message = "ログイン成功"
-        //TODO ログインが成功したらユーザの状態を保存
-        //TODO ログイン後のページの転送
-    } else {
-        // TODO ログイン画面に戻す
-    }
-    res.send(message)
-})
-
-//ctrl + c
-//サーバ待機
-app.listen(PORT,HOST, () => {
+//　サーバ停止: 起動中のターミナルで Ctrl + C
+// サーバ待機（Listen）
+app.listen(PORT, HOST, () => {
     console.log(HOST)
     console.log(PORT)
     console.log('wait...')
